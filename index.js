7ef3ca81e91d0390eeab8352cfc4445c9916b1bb
Ruben Villalvazo <vim.ruben@hotmail.com>
2022-12-28 00:19:49 -0300

    Main js structure

---
+++ index.js
@@ -0,0 +1,22 @@
+const cardObjectDefinitions = [
+    {
+        id: 1,
+        imagePath: '/assets/card-KingHearts.png'
+    },
+    {
+        id: 2,
+        imagePath: '/assets/card-JackClubs.png'
+    },
+    {
+        id: 3,
+        imagePath: '/assets/card-QueenDiamonds.png'
+    },
+    {
+        id: 4,
+        imagePath: '/assets/card-AceSpades.png'
+    }
+];
+
+function createCard(cardItem) {
+
+}
